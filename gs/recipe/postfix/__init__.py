1d030e4e7e2c9d6780a14ed7a15fde4ef3f3cb07
Michael JasonSmith <mpj17@onlinegroups.net>
2014-05-15 17:11:24 -0700

    Unicode update

--- gs/recipe/postfix/__init__.py
+++ gs/recipe/postfix/__init__.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2012, 2014 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from __future__ import absolute_import, unicode_literals
 import os
 import sys
-from creator import ConfigurationCreator
+from zc.buildout import UserError
+from .creator import ConfigurationCreator
 
 
 class Recipe(object):
-    """zc.buildout recipe"""
+    """zc.buildout recipe to create the Postifx config file"""
 
     def __init__(self, buildout, name, options):
         self.buildout, self.name, self.options = buildout, name, options
@@ -25,12 +40,12 @@ class Recipe(object):
             if os.path.exists(self.fileName):
                 m = '''
 *********************************************************************
-Skipped: The setup script %s has already been run. If you want
-to run it again set the run-once option to false or delete
-%s
-*********************************************************************\n\n''' %\
-                    (self.name, self.fileName)
-                sys.stdout.write(m)
+Skipped: The setup script {0}} has already been run. If you want
+to run it again set the "run-once" option to false or delete
+{1}
+*********************************************************************\n\n'''
+                msg = m.format(self.name, self.fileName)
+                sys.stdout.write(msg)
                 retval = False
         return retval
 
@@ -49,11 +64,11 @@ to run it again set the run-once option to false or delete
                     self.options['smtp2gs_path'],
                     self.options['site'],
                     configFolder)
-            except OSError, e:
-                m = '{name}: Failed to create example Postfix configuration :'\
-                    'in "{dir}":\n{error}\n'
-                sys.stderr.write(m.format(name=self.name, dir=d, error=e))
-                sys.exit(1)
+            except OSError as e:
+                m = '{0}: Failed to create example Postfix configuration :'\
+                    'in "{1}":\n{2}'
+                msg = m.format(self.name, d, e)
+                UserError(msg)
             else:
                 self.mark_locked()
                 m = '\nExample Postfix configuration written to\n'
--- gs/recipe/postfix/creator.py
+++ gs/recipe/postfix/creator.py
@@ -1,38 +1,56 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2012, 2014 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from __future__ import unicode_literals
+import codecs
 import os
+UTF8 = 'utf-8'
 
 
 class ConfigurationCreator(object):
-    automagic = 'groupserver-automagic'
+    'Create the configuration for Postfix'
 
-    def __init__(self):
-        pass
+    AUTOMAGIC = 'groupserver-automagic'
+    ALIAS = 'groupserver.aliases'
+    VIRTUAL = 'groupserver.virtual'
 
     def create_config_folder(self, configFolder):
+        'Create the configuration folder'
         if not(os.path.isdir(configFolder)):
-            os.mkdir(configFolder, 0755)
-        assert os.path.isdir(configFolder), \
-            '{0} does not exist'.format(configFolder)
+            os.mkdir(configFolder, 0o755)
 
     def create_alias(self, smtp2gs, site, configFolder):
-        outFileName = '{0}/groupserver.aliases'.format(configFolder)
+        'Create the alias file'
+        outFileName = os.path.join(configFolder, self.ALIAS)
         m = '# Postfix aliases, created by GroupServer.\n# See aliases(5) '\
             'for more information on this file. For more\n# GroupServer '\
             'options see\n#    {smtp2gs} --help\n'
         alias = '{automagic}:  "|{smtp2gs} http://{site}"\n'
-        outText = (m + alias).format(automagic=self.automagic,
+        outText = (m + alias).format(automagic=self.AUTOMAGIC,
                                         smtp2gs=smtp2gs, site=site)
-        with file(outFileName, 'w') as outFile:
+        with codecs.open(outFileName, mode='w', encoding=UTF8) as outFile:
             outFile.write(outText)
         return outFileName
 
     def create_virtual(self, site, configFolder):
-        outFileName = '{0}/groupserver.virtual'.format(configFolder)
+        'Create the virtual file'
+        outFileName = os.path.join(configFolder, self.VIRTUAL)
         m = '# Postfix virtual host setup, created by GroupServer.\n# See '\
             'virtual(5) for more information on the file format.\n'
         virtual = '{site}  virtual\n@{site}  {automagic}\n'
-        outText = m + virtual.format(site=site, automagic=self.automagic)
-        with file(outFileName, 'w') as outFile:
+        outText = m + virtual.format(site=site, automagic=self.AUTOMAGIC)
+        with codecs.open(outFileName, mode='w', encoding=UTF8) as outFile:
             outFile.write(outText)
         return outFileName
 
