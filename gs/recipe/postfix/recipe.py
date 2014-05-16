bb15199110cc4738518e7f27dd3d8474332b1f2c
Michael JasonSmith <mpj17@onlinegroups.net>
2014-05-16 14:03:57 -0700

    Moving the recipe out of __init__.py and into recipe.py.

---
+++ gs/recipe/postfix/recipe.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2012, 2014 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from __future__ import absolute_import, unicode_literals
+import os
+import sys
+from zc.buildout import UserError
+from .creator import ConfigurationCreator
+from gs.recipe.base import Recipe
+
+
+class PostfixConfigRecipe(Recipe):
+    """zc.buildout recipe to create the Postifx config file"""
+    POSTFIX_CONF_DIR = 'postfix_config'
+
+    def install(self):
+        """Installer"""
+        if self.should_run():
+            d = self.buildout['buildout']['directory']
+            configFolder = os.path.join(d, self.POSTFIX_CONF_DIR)
+            try:
+                configCreator = ConfigurationCreator()
+                writtenFiles = configCreator.create(
+                    self.options['smtp2gs_path'],
+                    self.options['site'],
+                    configFolder)
+            except OSError as e:
+                m = '{0}: Failed to create example Postfix configuration :'\
+                    'in "{1}":\n{2}'
+                msg = m.format(self.name, d, e)
+                UserError(msg)
+            else:
+                self.mark_locked()
+                fns = '\n    '.join(writtenFiles)
+                m = '\nExample Postfix configuration written to    {0}\n'
+                msg = m.format(fns)
+                sys.stdout.write(msg)
+        return tuple()
+
+    def update(self):
+        """Updater"""
+        self.install()
