9be4f367ef2ebd63277045ab262db9c5a14a8fde
Michael JasonSmith <mpj17@onlinegroups.net>
2014-05-15 19:52:40 -0700

    Adding to the tests

--- gs/recipe/postfix/tests/creator.py
+++ gs/recipe/postfix/tests/creator.py
@@ -13,11 +13,13 @@
 #
 ##############################################################################
 from __future__ import absolute_import, unicode_literals
+import codecs
 import os
 from shutil import rmtree
 from tempfile import mkdtemp
 from unittest import TestCase
 from gs.recipe.postfix.creator import ConfigurationCreator
+UTF8 = 'utf-8'
 
 
 class TestCreator(TestCase):
@@ -31,16 +33,40 @@ class TestCreator(TestCase):
     def tearDown(self):
         rmtree(self.tempDir)
 
+    def file_test(self, filePath):
+        self.assertIn(self.tempDir, filePath)
+        self.assertTrue(os.path.isfile(filePath))
+
+    def file_content_test(self, filePath, contents=[]):
+        with codecs.open(filePath, mode='r', encoding=UTF8) as inFile:
+            data = inFile.read()
+        for c in contents:
+            self.assertIn(c, data)
+
     def test_create_config_folder(self):
         'Test the creation of the configuration folder'
         self.creator.create_config_folder(self.folder)
         self.assertTrue(os.path.isdir(self.folder))
 
-    def _test_create_alias(self):
+    def test_create_alias(self):
         'Test the creation of the alias-file'
+        r = self.creator.create_alias('smtp2gs', 'groups.example.com',
+                                        self.tempDir)
+        self.file_test(r)
+        self.file_content_test(r, ('smtp2gs', 'groups.example.com'))
 
-    def _test_create_virtual(self):
+    def test_create_virtual(self):
         'Test the creation of the virtual file'
+        r = self.creator.create_virtual('groups.example.com',
+                                        self.tempDir)
+        self.file_test(r)
+        self.file_content_test(r, ('groups.example.com', ))
 
-    def _test_create(self):
+    def test_create(self):
         'Test the create method.'
+        r0, r1 = self.creator.create('smtp2gs', 'groups.example.com',
+                                            self.folder)
+        self.file_test(r0)
+        self.file_content_test(r0, ('smtp2gs', 'groups.example.com'))
+        self.file_test(r1)
+        self.file_content_test(r1, ('groups.example.com', ))
